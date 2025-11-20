f3d9d0182be1490fc37d000fc1f7db3572b43ce7
unknown <govindvattikolla@gmail.com>
2025-11-20 16:48:11 +0530

    error fixed

--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import menuData from "../items.json";
-import "./itemPage.css";
+import "./ItemPage.css";
 
 export default function ItemPage() {
   const { id } = useParams();
